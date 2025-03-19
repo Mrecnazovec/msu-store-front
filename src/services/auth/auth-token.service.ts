05f75709fd3edcff7887d070a495d20c45226624
Aleksandr Salnikov <TaynbIy@gmail.com>
2025-03-19 21:44:07 +0500

    fix access token

--- src/services/auth/auth-token.service.ts
+++ src/services/auth/auth-token.service.ts
@@ -19,5 +19,7 @@ export const saveTokenStorage = (accessToken: string) => {
 }
 
 export const removeFromStorage = () => {
-	Cookies.remove('accessToken')
+	Cookies.remove(EnumTokens.ACCESS_TOKEN, {
+		domain: process.env.APP_DOMAIN
+	})
 }
