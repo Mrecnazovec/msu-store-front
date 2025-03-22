fcf12827bef5c455b9ed6ec3a56620808cc850d3
Aleksandr Salnikov <TaynbIy@gmail.com>
2025-03-22 22:34:42 +0500

    fix seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
 	siteUrl: 'https://msu-store.com',
 	generateRobotsTxt: true,
 	sitemapSize: 5000, // Лимит ссылок в одном файле карты сайта
 	changefreq: 'daily',
 	priority: 0.7,
-	exclude: ['/store', '/dashboard', '/dashboard/favorites', '/store/discounts/create', '/store/discounts'],
+	exclude: ['/store', '/dashboard', '/dashboard/*', '/store/*', 'server-sitemap.xml'],
 	robotsTxtOptions: {
 		policies: [
 			{
